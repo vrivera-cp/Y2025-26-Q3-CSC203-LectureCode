0c44ed5d11a1b1f8fb48159b56cf454a0ed8fa84
Vanessa <10712121+rivessa@users.noreply.github.com>
2026-04-01 11:37:39 -0700

    Add Module1/Lecture2 code

---
+++ src/Module1/Lecture2/Hello.java
@@ -0,0 +1,13 @@
+package Module1.Lecture2;
+
+public class Hello {
+    // Method Signature
+    // Access Modifier: public (other classes can see and use this method)
+    // Other Modifier(s): static (this method belongs to the class itself)
+    // Return Type: void (does not return a value)
+    // Name: main
+    // Parameter(s): String[] args (A single string array, named args)
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/Module1/Lecture2/VariablesAndExpressions.java
@@ -0,0 +1,15 @@
+package Module1.Lecture2;
+
+public class VariablesAndExpressions {
+    public static void main(String[] args) {
+        int x = 3;
+        double y = 1.5;
+        int z = (int) y; // Type cast
+
+        char letter = 'A';
+        boolean a = true;
+
+        int expression = (int) 3.5 * 2; // 6
+        boolean booleanExpression = (100 > 0) || (1000 / 0 < 0);
+    }
+}
