4e5c28101d3ab5f61756cf5d29051fd5e7ff7a9a
rivessa <10712121+rivessa@users.noreply.github.com>
2026-04-06 11:37:38 -0700

    Add src/Module1/Lecture4 code

--- src/Module1/Lecture4/Lists.java
+++ src/Module1/Lecture4/Lists.java
@@ -1,4 +1,33 @@
 package Module1.Lecture4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Lists {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>( // Or LinkedList<>()
+                List.of(1, 2, 3)
+        );
+
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+
+        numbers.remove(2); // Remove the value 3
+
+        System.out.println(numbers);
+
+        System.out.println(numbers.get(2));
+
+        // Print all values
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        // Print all values using indices
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+    }
 }
--- src/Module1/Lecture4/Maps.java
+++ src/Module1/Lecture4/Maps.java
@@ -1,4 +1,25 @@
 package Module1.Lecture4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Maps {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("mochi", 6);
+        map.put("harvest", 5);
+        map.put("pearl", 4);
+
+        System.out.println(map);
+        System.out.println(map.get("harvest"));
+
+        System.out.println(map.getOrDefault("pearl", -1));
+
+        map.remove("pearl");
+
+        System.out.println(map.getOrDefault("pearl", -1));
+
+        System.out.println(map.get("pearl")); // Error
+    }
 }
